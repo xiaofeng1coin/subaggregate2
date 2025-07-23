77c678d9f9037e4941d432b7a36b3329685a2084
xiaofeng1coin <xiaofeng1coin@gmail.com>
2025-07-23 21:53:30 +0800

    fix：修复app端删除订阅失灵问题、修复桌面端单条订阅勾选失灵问题

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -16,9 +16,9 @@ android {
         targetSdk = 34
         
         // [关键修改] versionCode 必须是整数，每次发布都要增加。
-        versionCode = 10001
+        versionCode = 10002
         // [关键修改] versionName 是显示给用户的，我们把它更新为三段式。
-        versionName = "1.0.1" 
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
